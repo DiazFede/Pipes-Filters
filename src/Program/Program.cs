1df261b811fcdff6cfd70f015978b76c804937cc
DiazFede <diazfedericoucu@gmail.com>
2023-10-24 16:05:30 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,22 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+        PictureProvider provider = new PictureProvider();
+        IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
+
+        IFilter filter1 = new FilterNegative();
+        IFilter filter2 = new FilterBlurConvolution();
+
+        IPipe pipe2 = new PipeNull();  // Pipe final que no hace nada
+        IPipe pipe1 = new PipeSerial(filter2, pipe2);  // Pipe 1 con FilterBlurConvolution
+        IPipe pipe0 = new PipeSerial(filter1, pipe1);  // Pipe 0 con FilterNegative
+
+        // Aplicar la secuencia de pipes y filtros
+        IPicture result = pipe0.Send(picture);
+
+        provider.SavePicture(result, @"PathToImageToSave.jpg");
+
+        Console.WriteLine("Proceso completado. Imagen guardada.");
         }
     }
 }
