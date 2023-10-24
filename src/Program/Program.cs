b1ab22af3f145a889b45cf619e01ec47e66e7271
DiazFede <diazfedericoucu@gmail.com>
2023-10-24 16:27:23 -0300

    Se creo el filtro para guardar las imagenes sin hacerle ningun cambio, errores no resueltos en el programm

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,23 +7,40 @@ namespace CompAndDel
     class Program
     {
         static void Main(string[] args)
-        {
-        PictureProvider provider = new PictureProvider();
-        IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
+        {   
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
 
-        IFilter filter1 = new FilterNegative();
-        IFilter filter2 = new FilterBlurConvolution();
+            // Crear filtros
+            IFilter filter1 = new FilterNegative();
+            IFilter filter2 = new FilterBlurConvolution();
 
-        IPipe pipe2 = new PipeNull();  // Pipe final que no hace nada
-        IPipe pipe1 = new PipeSerial(filter2, pipe2);  // Pipe 1 con FilterBlurConvolution
-        IPipe pipe0 = new PipeSerial(filter1, pipe1);  // Pipe 0 con FilterNegative
+            // Crear pipes
+            IPipe pipe2 = new PipeNull();  // Pipe final que no hace nada
+            IPipe pipe1 = new PipeSerial(filter2, pipe2);  // Pipe 1 con FilterBlurConvolution
+            IPipe pipe0 = new PipeSerial(filter1, pipe1);  // Pipe 0 con FilterNegative
 
-        // Aplicar la secuencia de pipes y filtros
-        IPicture result = pipe0.Send(picture);
+            // Insertar el filtro para guardar la imagen en el paso 1
+            string outputPathStep1 = @"PathToStep1.jpg";
+            IFilter saveImageFilter = new FilterSaveImage(outputPathStep1);
+            pipe0 = new PipeSerial(pipe0, saveImageFilter);
 
-        provider.SavePicture(result, @"PathToImageToSave.jpg");
+            // Aplicar la secuencia de pipes y filtros
+            IPicture result = pipe0.Send(picture);
 
-        Console.WriteLine("Proceso completado. Imagen guardada.");
+            Console.WriteLine("Imagen guardada en " + outputPathStep1);
+
+            // Continuar con la secuencia (paso 2)
+            result = pipe1.Send(result);
+
+            Console.WriteLine("Paso 2 completado");
+
+            // Guardar la imagen final
+            string outputPathFinal = @"PathToFinalImage.jpg";
+            provider.SavePicture(result, outputPathFinal);
+
+            Console.WriteLine("Proceso completado. Imagen final guardada en " + outputPathFinal);
+
         }
     }
 }
