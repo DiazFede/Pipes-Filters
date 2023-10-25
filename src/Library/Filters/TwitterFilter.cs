3f59470a451604ac78652d3cc1e6bbd0500922b1
DiazFede <diazfedericoucu@gmail.com>
2023-10-25 09:50:22 -0300

    TwitterFilter

--- src/Library/Filters/TwitterFilter.cs
+++ src/Library/Filters/TwitterFilter.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Drawing;
+using CompAndDel;
+using CompAndDel.Filters;
+using CompAndDel.Pipes;
 using Ucu.Poo.Twitter;
+
+namespace CompAndDel
+
+public class TwitterFilter : IFilter
+{
+    private TwitterImage twitterImage;
+
+    public TwitterFilter(TwitterImage twitterImage)
+    {
+        this.twitterImage = twitterImage;
+    }
+    public async Task ApplyAsync(IPicture picture)
+    {  
+        string result = twitterImage.PublishToTwitter(picture, path);
+    }
+}
+
+
+
